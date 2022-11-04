8cdc46025d014897c731e582172ac4fde34de7e6
Zachary Riedlshah <git@zacharyrs.me>
2022-11-04 17:15:46 +1300

    chore: slightly clarify the problem

--- models/a.py
+++ models/a.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.orm.collections import attribute_keyed_dict
 from sqlalchemy import Integer
 
 from models import Base
-from models.b import B as _B
+from models.b import B  # ideally I want to import this as `_B` too
 
 
 class A(Base):
@@ -11,7 +13,7 @@ class A(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    Bs: Mapped[dict[str, _B]] = relationship(
+    Bs: Mapped[dict[str, B]] = relationship(
         "B",
         back_populates="A",
         collection_class=attribute_keyed_dict("name"),
--- models/b.py
+++ models/b.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, ForeignKey, String
@@ -5,8 +7,9 @@ from sqlalchemy import Integer, ForeignKey, String
 from models import Base
 
 if TYPE_CHECKING:
-    # from models.a import A as A  # works
-    from models.a import A as A_alias  # fails
+    # I can't `import x as _x` - seems the `Mapped` type-hint breaks
+    # Works if you use change to just `import A` and change the type-hint on line 28
+    from models.a import A as _A
 
 
 class B(Base):
@@ -21,7 +24,7 @@ class B(Base):
         nullable=False,
     )
 
-    A: Mapped["A_alias"] = relationship(
+    A: Mapped["_A"] = relationship(
         "A",
         back_populates="Bs",
     )
